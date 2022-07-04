64d166cd6292886b78c30696bfc49a5d487f5679
Tom Bennett <tombennett86@hotmail.co.uk>
2022-07-04 16:02:19 +0100

    add license

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# opentelemetry-instrumentation-gcp-pubsub
+# OpenTelemetry GCP PubSub Instrumentation for Node.js
+
+This module provides automatic instrumentation for [`@google-cloud/pubsub`](https://github.com/@google-cloud/pubsub).
+
+## Installation
+
+```
+npm install --save @tommbee/opentelemetry-instrumentation-gcp-pubsub
+```
+
+## Usage
+Initiate this module as part of the `instrumentations` config in your Open Telemetry set up: [@opentelemetry/instrumentation](https://github.com/open-telemetry/opentelemetry-js/tree/main/packages/opentelemetry-instrumentation)
+
+```js
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { GCPPubSubInstrumentation } = require('@tommbee/opentelemetry-instrumentation-gcp-pubsub');
+
+registerInstrumentations({
+  tracerProvider,
+  instrumentations: [
+    new GCPPubSubInstrumentation({
+      subscriberSpanName: 'handle.subscription'
+    })
+  ]
+});
+```
+
+### Instrumentation Options
+
+| Options              | Description                                                                |
+|----------------------|----------------------------------------------------------------------------|
+| `subscriberSpanName` | The name of the Span created to trace events received through a subscriber |
+
+---
+
+[License](LICENSE)
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,7 +3,7 @@ import {
   InstrumentationBase,
   isWrapped,
   InstrumentationNodeModuleDefinition,
-  safeExecuteInTheMiddle, InstrumentationConfig,
+  safeExecuteInTheMiddle,
 } from '@opentelemetry/instrumentation';
 import {
   context,
@@ -14,13 +14,68 @@ import {
 } from '@opentelemetry/api';
 import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
 import { Subscription, Message } from '@google-cloud/pubsub';
+import { VERSION } from './version';
+import { GCPPubSubInstrumentationConfig } from './types';
 
 export class GCPPubSubInstrumentation extends InstrumentationBase<
   typeof PubSub
 > {
-  constructor(config: InstrumentationConfig = {}) {
-    console.log('pubsub instrucmentation constructor');
-    super('opentelemetry-instrumentation-gcp-pubsub', '0.0.1', { ...config });
+  protected override _config!: GCPPubSubInstrumentationConfig;
+
+  constructor(config: GCPPubSubInstrumentationConfig = {}) {
+    super(
+      'opentelemetry-instrumentation-gcp-pubsub',
+      VERSION,
+      Object.assign({}, config)
+    );
+  }
+
+  override setConfig(config: GCPPubSubInstrumentationConfig = {}) {
+    this._config = config;
+  }
+
+  protected init():
+    | InstrumentationNodeModuleDefinition<typeof PubSub>
+    | InstrumentationNodeModuleDefinition<typeof PubSub>[]
+    | void {
+    diag.debug('Initiating GCP PubSub Open Telemetry instrumentation');
+
+    return [
+      new InstrumentationNodeModuleDefinition<typeof PubSub>(
+        '@google-cloud/pubsub',
+        ['^3.0.0'],
+        this.patch.bind(this),
+        this.unpatch.bind(this)
+      ),
+    ];
+  }
+
+  private unpatch(
+    moduleExports: typeof PubSub,
+    moduleVersion: string | undefined
+  ) {
+    diag.debug(`Unpatching @google-cloud/pubsub@${moduleVersion}`);
+
+    if (isWrapped(moduleExports.Subscription.prototype.on)) {
+      this._unwrap(moduleExports.Subscription.prototype, 'on');
+    }
+  }
+
+  private patch(
+    moduleExports: typeof PubSub,
+    moduleVersion: string | undefined
+  ) {
+    diag.debug(`Applying patch for @google-cloud/pubsub@${moduleVersion}`);
+
+    if (!isWrapped(moduleExports.Subscription.prototype.on)) {
+      this._wrap(
+        moduleExports.Subscription.prototype,
+        'on',
+        this._getOnPatch.bind(this)
+      );
+    }
+
+    return moduleExports;
   }
 
   private _getOnPatch(original: Subscription['on']) {
@@ -30,7 +85,6 @@ export class GCPPubSubInstrumentation extends InstrumentationBase<
       eventName: string | symbol,
       listener: (...args: any[]) => void
     ) {
-      console.log('patching listener');
       if (eventName === 'message') {
         const originalListener = listener;
 
@@ -40,7 +94,7 @@ export class GCPPubSubInstrumentation extends InstrumentationBase<
             message.attributes || {}
           );
           await self.tracer.startActiveSpan(
-            `${this.name} process`,
+            self._config.subscriberSpanName || `${this.name} process`,
             {
               kind: SpanKind.CONSUMER,
               attributes: {
@@ -48,7 +102,7 @@ export class GCPPubSubInstrumentation extends InstrumentationBase<
                 deliveryAttempt: message.deliveryAttempt,
                 [SemanticAttributes.MESSAGING_SYSTEM]: 'pubsub',
                 [SemanticAttributes.MESSAGING_OPERATION]: 'process',
-                [SemanticAttributes.MESSAGING_DESTINATION]: original.name,
+                [SemanticAttributes.MESSAGING_DESTINATION]: this.name,
                 [SemanticAttributes.MESSAGING_DESTINATION_KIND]: 'topic',
                 [SemanticAttributes.MESSAGING_MESSAGE_ID]: message.id,
                 [SemanticAttributes.MESSAGING_PROTOCOL]: 'pubsub',
@@ -71,59 +125,18 @@ export class GCPPubSubInstrumentation extends InstrumentationBase<
                   false
                 );
               } catch (err: unknown) {
-                span.setStatus({
-                  code: SpanStatusCode.ERROR,
-                  message: String(err),
-                });
+                span
+                  .setStatus({
+                    code: SpanStatusCode.ERROR,
+                    message: String(err),
+                  })
+                  .end();
               }
-              span.end();
             }
           );
         };
       }
       return original.apply(this, [eventName, listener]);
     };
-  }
-
-  private unpatch(
-    moduleExports: typeof PubSub,
-    moduleVersion: string | undefined
-  ) {
-    if (isWrapped(moduleExports.Subscription.prototype.on)) {
-      this._unwrap(moduleExports.Subscription.prototype, 'on');
-    }
-  }
-
-  private patch(
-    moduleExports: typeof PubSub,
-    moduleVersion: string | undefined
-  ) {
-    console.log('patching');
-    diag.debug(`Applying patch for @${moduleVersion}`);
-    if (!isWrapped(moduleExports.Subscription.prototype.on)) {
-      console.log('wrapping');
-      this._wrap(
-        moduleExports.Subscription.prototype,
-        'on',
-        this._getOnPatch.bind(this)
-      );
-    }
-
-    return moduleExports;
-  }
-
-  protected init():
-    | InstrumentationNodeModuleDefinition<typeof PubSub>
-    | InstrumentationNodeModuleDefinition<typeof PubSub>[]
-    | void {
-    diag.debug(`Initiating PubSub instrumentation`);
-    return [
-      new InstrumentationNodeModuleDefinition<typeof PubSub>(
-        '@google-cloud/pubsub',
-        ['^3.0.0'],
-        this.patch.bind(this),
-        this.unpatch.bind(this)
-      ),
-    ];
   }
 }
